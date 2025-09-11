51fe3942ff24cfa17abce0bc68bc355d897e907c
thinkpad <thinkpad@example.com>
2025-09-11 20:18:11 +0800

    modify bat

--- vimrc/grap.bat
+++ vimrc/grap.bat
@@ -1,21 +1,18 @@
  adb shell "su -c 'ps -A | grep logcat'"
  adb shell "su -c 'ps -A | grep screenrecord'"
  adb shell "su -c 'pkill -f logcat'"
- adb shell "su -c 'pkill -f screenrecord'"
- adb shell "su -c 'ls -la /sdcard/demo.mp4'"
  adb shell "su -c 'ls -la /sdcard/bluetooth_debug.log'"
- adb shell "su -c 'rm -rf /sdcard/demo.mp4'"
  adb shell "su -c 'rm -rf /sdcard/bluetooth_debug.log'"
+ adb shell "su -c 'rm -rf /sdcard/Movies/*'"
+
 
  adb logcat -G 256m
  adb logcat -g
  adb shell "su -c 'logcat -c '"
  adb shell "su -c 'nohup logcat -b all -v threadtime -f /sdcard/bluetooth_debug.log >/dev/null 2>&1 &'"
- adb shell "su -c 'screenrecord /sdcard/demo.mp4 &>/dev/null &'"
 
- adb shell "su -c 'ls -la /sdcard/bluetooth_debug.log'"
- adb shell "su -c 'ls -la /sdcard/demo.mp4'"
  adb shell "su -c 'ps -A | grep logcat'"
  adb shell "su -c 'ps -A | grep screenrecord'"
+ adb shell "su -c 'ls -la /sdcard/bluetooth_debug.log'"
 
 
--- vimrc/pull.bat
+++ vimrc/pull.bat
@@ -1,13 +1,10 @@
- adb shell "su -c 'ps -A | grep logcat'"
- adb shell "su -c 'ps -A | grep screenrecord'"
- adb shell "su -c 'pkill -f logcat'"
- adb shell "su -c 'pkill -f screenrecord'"
+
  
  adb shell "su -c 'ls -la /data/misc/bluetooth/logs'"
  adb shell "su -c 'ls -la /sdcard/bluetooth_debug.log'"
- adb shell "su -c 'ls -la /sdcard/demo.mp4'"
 
-
+ adb shell "su -c 'ps -A | grep logcat'"
+ adb shell "su -c 'pkill -f logcat'"
 
 
  adb shell "su -c 'rm -rf /sdcard/logs'"
@@ -19,5 +16,6 @@
  md log
  adb pull /sdcard/logs ./log/logs
  adb pull /sdcard/bluetooth_debug.log  ./log/bluetooth_debug.log
- adb pull /sdcard/demo.mp4  ./log/demo.mp4
+ adb pull /sdcard/Movies  ./log/Movies
+ 
  
